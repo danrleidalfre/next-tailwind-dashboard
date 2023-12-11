f81460b0a00250918059a62f9c8818328d8b3617
Danrlei <danrleidalfre@gmail.com>
2023-12-11 13:20:41 -0300

    finished sidebar and implement composition pattern in input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>main</h1>
+  return <div />
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,14 +1,20 @@
 import { Logo } from '@/components/Sidebar/Logo'
 import { Nav } from '@/components/Sidebar/Nav'
-import { Search } from '@/components/Sidebar/Search'
 import { Footer } from '@/components/Sidebar/Footer'
 import { Profile } from '@/components/Sidebar/Profile'
+import * as Input from '@/components/Input'
+import { Search } from 'lucide-react'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-      <Search />
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
       <Nav />
       <Footer />
       <div className="h-px bg-zinc-200" />
