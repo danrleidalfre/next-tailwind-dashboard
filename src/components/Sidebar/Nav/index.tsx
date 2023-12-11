bac2e9c74bf793e1396f8900c21a8ea82b912023
Danrlei <danrleidalfre@gmail.com>
2023-12-11 10:43:10 -0300

    implement navigation menu aside

---
+++ src/components/Sidebar/Nav/index.tsx
@@ -0,0 +1,22 @@
+import { NavItem } from '@/components/Sidebar/NavItem'
+import {
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+
+export function Nav() {
+  return (
+    <nav className="space-y-0.5">
+      <NavItem title="Home" icon={Home} />
+      <NavItem title="Dashboard" icon={BarChart} />
+      <NavItem title="Projects" icon={SquareStack} />
+      <NavItem title="Tasks" icon={CheckSquare} />
+      <NavItem title="Reporting" icon={Flag} />
+      <NavItem title="Users" icon={Users} />
+    </nav>
+  )
+}
