a3fc3d44bdab384dce51297478e581c6232757cf
Danrlei <danrleidalfre@gmail.com>
2023-12-12 21:07:09 -0300

    implement file input

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,13 @@
 import { Tabs } from '@/components/Main/Tabs'
 import { PageTitle } from '@/components/Main/PageTitle'
+import { Form } from '@/components/Main/Form'
 
 export default function Main() {
   return (
-    <main className="px-4 pb-12 pt-8">
+    <main className="px-8 pb-12 pt-8">
       <PageTitle title="Settings" />
       <Tabs />
+      <Form />
     </main>
   )
 }
